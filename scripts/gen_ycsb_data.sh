cd753510be5b0ae9aa3d499c95b179c1b93bc86e
Dumi Loghin <dumiloghin@gmail.com>
2021-11-07 16:18:03 +0800

    generate workloads with different kv size

--- scripts/gen_ycsb_data.sh
+++ scripts/gen_ycsb_data.sh
@@ -94,6 +94,14 @@ echo "Workload C..."
 bin/ycsb.sh load basic -P $WLOAD_DIR/zipfian/workloadc -p recordcount="${RECORD_COUNT}"> "${WORK_DIR}"/ycsb_data_zipfian/workloadc.dat
 bin/ycsb.sh run basic -P $WLOAD_DIR/zipfian/workloadc -p recordcount="${RECORD_COUNT}" -p operationcount="${OPERATION_COUNT}"> "${WORK_DIR}"/ycsb_data/run_workloadc.dat
 
+# Different Record Sizes
+SIZES="512B 2kB 8kB 32kB 128kB"
+for SIZE in $SIZES; do
+  mkdir -p "${WORK_DIR}"/ycsb_data_$SIZE
+  bin/ycsb.sh load basic -P $WLOAD_DIR/sizes/workloada_$SIZE -p recordcount="${RECORD_COUNT}"> "${WORK_DIR}"/ycsb_data_$SIZE/workloada.dat
+  bin/ycsb.sh run basic -P $WLOAD_DIR/uniform/workloada_$SIZE -p recordcount="${RECORD_COUNT}" -p operationcount="${OPERATION_COUNT}"> "${WORK_DIR}"/ycsb_data_$SIZE/run_workloada.dat
+done
+
 # shellcheck disable=SC2164
 popd
 # shellcheck disable=SC2164
