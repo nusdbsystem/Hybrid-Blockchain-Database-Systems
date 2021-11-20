558e4881b533e8c24ad0cbcc23f0b8a7197d62c7
tianwen <tianwenw.vk@gmail.com>
2021-11-20 13:49:29 +0800

    kill dead containers

--- scripts/kill_containers_blockchaindb.sh
+++ scripts/kill_containers_blockchaindb.sh
@@ -7,7 +7,7 @@ IMGNAME="blockchaindb"
 PREFIX="blockchaindb"
 
 idx=1
-for id in `docker ps | grep $PREFIX | cut -d ' ' -f 1`; do
+for id in `docker ps -a| grep $PREFIX | cut -d ' ' -f 1`; do
 	echo "$PREFIX$idx"
 	idx=$(($idx+1))
 	docker kill $id
